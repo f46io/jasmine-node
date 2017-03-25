b03033a25918449473ef311c2f83e49f6bfbb2a0
f46io <f46iol@gmail.com>
2017-03-25 22:48:21 +0100

    Arquivos do projeto local

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by f46io on 24/03/2017.
+ */
+describe("suite de teste como exemplo com o node js", function () {
+    it("deve validar o uso do jasmine com o noede js", function () {
+        expect(true).toEqual(true);
+        }) ;
+
+    it("deve demonstrar o uso do karma escuta", function () {
+        expect(1).toEqual(1);
+    });
+});
